b118504066dfb9ac2d5b7f4f62d584e50cf3239a
Myron <dubtwee@gmail.com>
2020-04-07 10:40:24 -0500

    testing console log to demo browser console is still available

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// on server
+// const React = require('react');
 
 function App() {
+  console.log('HEY YO!!!');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to RELOAD.
         </p>
         <a
           className="App-link"
